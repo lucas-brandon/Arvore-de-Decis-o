28bf1c8c34175e82b13c59b368db6c47751a8daf
lucas-brandon <lucasbrand_on@hotmail.com>
2020-04-23 15:01:47 -0300

    Update Decision.py

--- Decision.py
+++ Decision.py
@@ -1,107 +1,96 @@
 import numpy
 
 
+#Função para transferir os dados do arquivo para a estrutura do algoritmo,
+#cada linha possui um valor para cada atributo de entrada e um valor para saída
+#A função recebe um conjunto de dados "f" e quantas linhas "numExamples" serão utilizadas
 def InitExamples(f, numExamples):
     l = f.readlines()
-    a = []
-
+    a = []    
     
-    
-    for x in l:
+    for x in range(len(l)):
         newLine = []
         rChar = ""
-        for y in x:
-            
-            #print("char " + y + "\n")
-            if y == ',':
+        
+        for y in range(len(l[x])):
+            if l[x][y] == ',':
                 aux1 = float(rChar)
                 newLine.append(aux1)
                 rChar = ""
 
-            elif y == x[-1]:
+            elif l[x][y] == '\n':
                 newLine.append(rChar)
                 rChar = ""
-                #print("a")
+
+            elif x == len(l)-1 and y == len(l[x])-1:
+                rChar += l[x][y]
+                newLine.append(rChar)
 
             else:
-                rChar += y
-
+                rChar += l[x][y]
+                
         if len(a) < numExamples:
             a.append(newLine)
 
-    print("tam f: ", len(a))
+    #print("tam f: ", len(a))
     return a
 
+#Cálculo da informação obtida utilizando um determinado atributo
+#A função recebe um conjunto de exemplos e o atributo escolhido
+#Para comparar cada valor possivel do atributo, a função divide os valores em dois conjuntos:
+#conjunto de valores menor do que a média, e conjunto de valores maior/igual a média
+#
 def Remainder(examples, atribbute):
 
     remainder = 0
-
-    atVal = {0.0}
     out = {""}
-    atValues = []
     output = []
 
     meanCount = 0    
     for x in range(len(examples)):
-        #print(x)
-        atVal.add(examples[x][atribbute])
+        #print("ex ", examples[x])
         out.add(examples[x][-1])
         meanCount += examples[x][atribbute]
 
     
     meanVal = meanCount/(len(examples))
-
+    #meanVal = round(meanVal, 3)
+    #print("mean remainder: ", meanVal)
     
-    for x in atVal:
-        atValues.append(x)
     for x in out:
         output.append(x)
 
-    del atValues[0]
+    #print("all outputs: ", out)
     del output[0]
 
     #Every possible value of A
     #Divide all values comparing to the mean 
 
-    
-    #print("atValues: ", atValues)
-    #print("output: ", output)
-
-    #i = 2 vezes (abaixo/acima da média)
-    #for i in range(len(atValues)-1):
-
     exValuesLess = []
-    exValuesMore = []
-    #print("var: ", atValues[i])
-    #filtrar o vetor original para um vetor
-    #apenas com o valor da var
-    for j in range(len(examples)):
+    exValuesGreater = []
 
-        #print(atValues[i], " = ", examples[j][atribbute])
+    for j in range(len(examples)):
         if examples[j][atribbute] < meanVal:
-        
             aux = []
             aux.append(examples[j][atribbute])
             aux.append(examples[j][-1])
             exValuesLess.append(aux)
+            
         else:            
             aux = []
             aux.append(examples[j][atribbute])
             aux.append(examples[j][-1])
-            exValuesMore.append(aux)
+            exValuesGreater.append(aux)
 
     outProbLess = []
-    outProbMore = []
-
-    print("example values less: ", len(exValuesLess))
-    print("example values less: ", len(exValuesMore))
+    outProbGreater = []
     
-    #print("exValues: ", exValues)
     countTL = 0
-    countTM = 0
+    countTG = 0
     for j in range(len(output)):
         countL = 0
-        countM = 0
+        countG = 0
+
         for k in range(len(exValuesLess)):
             if output[j] == exValuesLess[k][-1]:
                 countL+=1
@@ -109,25 +98,17 @@ def Remainder(examples, atribbute):
         outProbLess.append(countL)
         countTL += countL
 
-        for k in range(len(exValuesMore)):
-            if output[j] == exValuesMore[k][-1]:
-                countM+=1
+        for k in range(len(exValuesGreater)):
+            if output[j] == exValuesGreater[k][-1]:
+                countG+=1
 
-        outProbMore.append(countM)
-        countTM += countM
-    
+        outProbGreater.append(countG)
+        countTG += countG
+
     outProbLess[:] = [x / countTL for x in outProbLess]
-    outProbMore[:] = [x / countTM for x in outProbMore]
+    outProbGreater[:] = [x / countTG for x in outProbGreater]
 
-    
-    #print("ProbM = ", len(outProbMore))
-    #print("ProbL = ", len(outProbLess))
-    
-            
-    #print("prob: ", outProb)
-    #print("total: ", countT)
-
-    #Calcular I()
+    #Information on values less than mean
 
     r1 = 0
     c = 0
@@ -135,103 +116,83 @@ def Remainder(examples, atribbute):
         if j != 0:
             c += (-1)*j*numpy.log2(j)
 
-    r1 = (countTL/(len(examples)-1))*c
+    r1 = (countTL/(countTL+countTG))*c
 
-   
-
-    #Calcular I()
+    #Information on values greater than mean
 
     r2 = 0
     c = 0
-    for j in outProbMore:
+    for j in outProbGreater:
         if j != 0:
             c += (-1)*j*numpy.log2(j)
 
-    r2 = (countTM/(len(examples)-1))*c
-
-   
+    r2 = (countTG/(countTL+countTG))*c
     
     remainder = r1 + r2
-
-    #print("valor A: ", r)
 
     #print("remainder A = ", remainder)
     return remainder
 
-        
-
-
-
+#Cálculo da informação contida no conjunto de exemplos
+#A função recebe o conjunto de exemplos, com os valores das entradas e saída
 
 def InfoGlobal(examples):
 
-    #Mapear as possibilidades de saída
-
+    #Find all target outputs
     targets = {""}
 
     for i in examples:
         targets.add(i[-1])
 
-    #print(targets)
-
     target = []
     for x in targets:
         target.append(x)
 
+    del target[0]
     probTarget = []
 
-    
-    
     for x in target:
         count = 0
+        
         for y in examples:
-            #print(len(y))
             if x == y[-1]:
                 count+=1
 
         probTarget.append(count)
 
-    #print(probTarget)
-
-    del probTarget[0]
-
     probTarget[:] = [x / len(examples) for x in probTarget]
-    
 
-    #print(probTarget)    
-
-    #Calcular Informação
+    #Information
 
     ig1 = 0
     for x in probTarget:
-        k = numpy.log2(x)
-        ig1+=x*k*(-1)
+        ig1 += x*numpy.log2(x)*(-1)
 
-    #print(ig1)
     return ig1
 
-
+#Classe auxiliar para representar um nó na árvore de decisão
+#Variável "mode" possui o valor 0 para representar um nó com valor da saída,
+#e possui o valor 1 para um nó atributo
+#Variável "atribbute" armazena o valor da saída ou um atributo (o índice do atributo)
+#Variável "branches" representa o valor da média dos valores de um atributo, que divide um conjunto em dois
+#Variável "nextNodes[]" armazena os nós adjacentes. Usando a média, existem apenas 2 nós adjacentes.
 class Node:
     
     def __init__(self, mode, atribbute):
-        #self.root = root
         self.mode = mode
         self.atribbute = atribbute
         self.branches = -1
         self.nextNodes = []
         
 
-
+#Algoritmo ID3
+#Constrói uma árvore de decisão, usando a heurística de escolher o atributo com maior
+#maior ganho de informação        
 def ID3(examples, validAtr):
 
-    #Descobrir valor de saída mais comum
-
     targets = {""}
-
     for i in examples:
         targets.add(i[-1])
-    print("len examples: ", len(examples))
-    print("len targets: ", len(targets))
 
     target = []
     for x in targets:
@@ -241,20 +202,18 @@ def ID3(examples, validAtr):
     
     countOutput = 0
     commonOutput = 0
-    for i in range(len(target) - 1):
+    for i in range(len(target)):
         x = examples.count(target[i])
         if x > countOutput:
             x = countOutput[i]
             commonOutput = i
             
-
+    #print("targets: ", target, "\n")
+    
     if len(target) < 2:
-        print("node with only 1 target\n")
-        print("target: ", len(target))
         n = Node(0, target[-1])
         
     elif validAtr.count(1) == 0:
-        print("node with only 1 atr\n")
         n = Node(0, target[commonOutput])
         
     else:
@@ -264,112 +223,82 @@ def ID3(examples, validAtr):
         for i in range(len(validAtr)):
             if validAtr[i] == 1:
                 a = InfoGlobal(examples)
-
                 b = Remainder(examples, i)
-
                 c = a-b
-
-                print("global info: ", a, " remainder: ", b, " igl: ", c)
 
                 if c > bestIGL:
                     bestIGL = c
-                    bestAtr = i
-                #print("Atb ", count2," = ", a, " - ", b, " = ", c)
-                #count2+=1
+                    bestAtr = i                
 
         n = Node(1, bestAtr)
-        validAtr[bestAtr] = 0
+        validAtr1 = validAtr.copy()
         validAtr2 = validAtr.copy()
 
+        validAtr1[bestAtr] = 0
+        validAtr2[bestAtr] = 0
+
         print("best atr: ", bestAtr,"\n")
+        #print(validAtr1)
+        #print(validAtr2)
 
         #Every possible value of A
         #Divide all values comparing to the mean
-        atVal = {0.0}
-        atValues = []
-
         
         meanCount = 0
-        for i in range(len(examples)-1):
-            #print(len(i))
-            atVal.add(examples[i][bestAtr])
+        for i in range(len(examples)):
             meanCount += examples[i][bestAtr]
 
         #Calculate the mean value
         meanVal = meanCount/(len(examples))
 
-        print("mean: ", meanVal, "\n")
-
-        #print("atval: ", atVal)
-        for i in atVal:
-            atValues.append(i)
-
-
-        #print("mean: ", meanVal)
+        #print("mean id3: ", meanVal, "\n")
+        
         newExamples1 = []
         newExamples2 = []
 
-        #branch < mean
         n.branches = meanVal
         
         n.nextNodes.append(Node(0, commonOutput))
         n.nextNodes.append(Node(0, commonOutput))
         
-        for j in range(len(examples) - 1):
+        for j in range(len(examples)):
             if examples[j][bestAtr] < meanVal:
                 newExamples1.append(examples[j])
             else:
                 newExamples2.append(examples[j])
 
-        #print("valid atr 1: ", validAtr)                
-        #print("new examples less: ", len(newExamples1))
-        #print("new examples more: ", len(newExamples2))
-        print("less\n")
-        if len(newExamples1) > 0:
-            newNode1 = ID3(newExamples1, validAtr)
+        if len(newExamples1) >= 0:
+            newNode1 = ID3(newExamples1, validAtr1)
             n.nextNodes[0] = newNode1
 
-        #print("valid atr 2: ", validAtr)                
-        print("more\n")
-        if len(newExamples2) > 0:
+        if len(newExamples2) >= 0:
             newNode2 = ID3(newExamples2, validAtr2)
             n.nextNodes[1] = newNode2
 
-        #print("n1 : ", newNode1)
-        #print("n2 : ", newNode2)
-        
+        #print(n.atribbute)
+        #print(n.nextNodes[0].atribbute, " and ", n.nextNodes[1].atribbute)
 
-        #for i in range(len(atValues) - 1):
-            #n.branches.append(atValues[i])
-            #n.nextNodes.append(Node(0, commonOutput))
-
-            #for j in range(len(examples) - 1):
-                #if examples[j][bestAtr] == atValues[i]:
-                    #newExamples.append(examples[j])
-
-            #newNode = ID3(newExamples, validAtr)
-            #n.nextNodes[i] = newNode
+        if n.nextNodes[0].mode == n.nextNodes[1].mode and n.nextNodes[0].atribbute == n.nextNodes[1].atribbute and n.nextNodes[0].mode == 0: 
+            n.mode = n.nextNodes[0].mode
+            n.atribbute = n.nextNodes[0].atribbute
 
     return n
 
-
+#Função para validar a árvore de decisão
+#A função recebe o conjunto de testes e a árvore de decisão.
+#Cada exemplo de teste é avaliado usando os nós da árvore e comparado com o valor de saída alvo.
+#Ao final do teste, a precisão dos resultados é apresentada
 def Validate(test, node):
     count = 0
     for i in range(len(test)):
         print("teste ", i)
         s = node
         while s.mode != 0:
-            #print(s.atribbute)
             if s.mode == 1:
                 if test[i][s.atribbute] < s.branches:
                     s = s.nextNodes[0]
                 else:
                     s = s.nextNodes[1]
-            else:
-                if s.nextNodes[0] != 0:
-                    print("res = ", s.nextNodes[0])
-                else:
-                    print("res = ", s.nextNodes[1])
 
         print(s.atribbute)
         if s.atribbute == test[i][-1]:
@@ -379,12 +308,38 @@ def Validate(test, node):
             print("---------wrong")
     print("precision: ", count/len(test))
 
-#f=open("iris.data", "r")
+#Exibir a árvore de decisão encontrada pelo algoritmo ID3, usando uma busca BFS
+#para visitar os nós
+def ShowTree(s, atrNames):
 
+    queue =[s]
+    print("\nfinal tree\n")
+    while len(queue) > 0:
+        n = queue[0]
+        del queue[0]
+        #print("queue length: ", len(queue))
+        print("n = ", n.atribbute, " = ", atrNames[n.atribbute])
+        print("branch(mean) = ", n.branches)
+        print("nodes adj = ", len(n.nextNodes))
+
+            
+        for i in range(len(n.nextNodes)):
+                
+            #print("node adj: ", n.nextNodes[i].atribbute)
+                
+            if n.nextNodes[i].mode != 0:
+                queue.append(n.nextNodes[i])
+                print("node (", n.nextNodes[i].atribbute,") adj: ", atrNames[n.nextNodes[i].atribbute])
+            else:
+                print("node adj: ", n.nextNodes[i].atribbute)
+        print()
+
+#Instruções para leitura de um arquivo de dados para treino e outro para testes
 f=open("iris3.data", "r")
 g=open("irisTest.data", "r")
 
 
+#Função principal para adaptar o programa para os dados do "Iris Data Set"
 if f.mode == 'r':
         l = InitExamples(f, 130)
 
@@ -394,51 +349,12 @@ if f.mode == 'r':
         for x in range(numAtr):
             validAtr.append(1)
 
-        count = 0
-        count2 = 0
 
         atrNames = ["Sepal Length", "Sepal Width", "Petal Length", "Petal Width",]
 
         s = ID3(l, validAtr)
-
-        #input()
-
-        queue =[s]
-
-        while len(queue) > 0:
-            n = queue[0]
-            del queue[0]
-            #print("queue length: ", len(queue))
-            print("n = ", n.atribbute, " = ", atrNames[n.atribbute])
-            print("branches = ", n.branches)
-            print("nodes adj = ", len(n.nextNodes))
-
-            
-            for i in range(len(n.nextNodes)):
-                
-                #print("node adj: ", n.nextNodes[i].atribbute)
-                
-                if n.nextNodes[i].mode != 0:
-                    queue.append(n.nextNodes[i])
-                    print("node adj: ", atrNames[n.nextNodes[i].atribbute])
-                else:
-                    print("node adj: ", n.nextNodes[i].atribbute)
+        
+        ShowTree(s, atrNames)
 
         test = InitExamples(g, 20)
-        Validate(test, s)                    
-        print("\n")
-
-        #while len(l) != 0:
-        #while count2 < numAtr:
-
-            #a = InfoGlobal(l)
-
-            #b = Remainder(l, count2)
-
-            #c = a-b
-            #print("Atb ", count2," = ", a, " - ", b, " = ", c)
-            #count2+=1
-
-            
-
-        
+        Validate(test, s)
